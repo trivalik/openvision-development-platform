32608e8677703ad115e5f4bdbca8ca2c37850f3b
persianpros <persianpros@yahoo.com>
2020-01-01 18:41:14 +0330

    meta-uclan remove upcoming branch

--- submodule.sh
+++ submodule.sh
@@ -32,7 +32,7 @@ then
 	read SUBMODULENAME
 	echo -e "${NC}"
 	cd meta-${SUBMODULENAME}
-	if [ ${SUBMODULENAME} = "dream" -o ${SUBMODULENAME} = "vuplus" -o ${SUBMODULENAME} = "sh4" -o ${SUBMODULENAME} = "uclan" ];then
+	if [ ${SUBMODULENAME} = "dream" -o ${SUBMODULENAME} = "vuplus" -o ${SUBMODULENAME} = "sh4" ];then
 		echo "Checking out meta-${SUBMODULENAME} upcoming branch:"
 		git checkout upcoming
 	else
@@ -327,8 +327,8 @@ then
 	echo -e "\n"
 	cd ..
 	cd meta-uclan
-	echo "Checking out meta-uclan upcoming branch:"
-	git checkout upcoming
+	echo "Checking out meta-uclan develop branch:"
+	git checkout develop
 	git pull
 	echo -e "\n"
 	cd ..
