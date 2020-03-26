bb456556670effe18554d015f1c72a000456d911
Persian Prince <persianpros@yahoo.com>
2020-03-26 22:41:42 +0430

    Update image.sh

--- image.sh
+++ image.sh
@@ -9,6 +9,8 @@ VISIONVERSION=`cat meta-openvision/conf/distro/openvision-common.conf | grep -oP
 VISIONREVISION=`cat meta-openvision/conf/distro/openvision-common.conf | grep -oP '(?<=VISIONREVISION = "r)[0-9]*'`
 echo -e "${BLUE}Welcome to Open Vision ${GREEN}${VISIONVERSION}-r${VISIONREVISION} ${BLUE}image compile script!"
 echo -e ""
+echo -e "${RED}Notice: this script is case sensitive!${NC}"
+echo -e ""
 echo -e "First we need to check your Ubuntu 18.04.x"
 echo -e ""
 if [ -f user.ovstep ]; then
