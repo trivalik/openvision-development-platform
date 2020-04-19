edacd2490f0cf8ec0a9a0c32a75586a426b0c70c
persianpros <persianpros@yahoo.com>
2020-04-19 22:07:54 +0430

    Update partitions-by-name

--- meta-openvision/recipes-support/partitions-by-name/files/partitions-by-name.sh
+++ meta-openvision/recipes-support/partitions-by-name/files/partitions-by-name.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-search=/sys/block/mmcblk0/mmcblk0p*
-for i in $search;
+function search {
+for i in $1;
 do
-  if [ "$i" != "$search" ]; then
+  if [ "$i" != "$1" ]; then
     partname=`cat /$i/uevent | grep PARTNAME | cut -d '=' -f 2`
     devname=`cat /$i/uevent | grep DEVNAME | cut -d '=' -f 2`
     mkdir -p /dev/block/by-name/
-    ln -sf /dev/$devname /dev/block/by-name/$partname
+    if [ ! -e /dev/block/by-name/$partname ]; then
+      ln -sf /dev/$devname /dev/block/by-name/$partname
+    else
+      ln -sf /dev/$devname /dev/block/by-name/$partname-$devname
+    fi
   fi
 done
+}
+
+if [ -d "/sys/block/mmcblk0" ]; then
+    search "/sys/block/mmcblk0/mmcblk0p*"
+fi
+if [ -d "/sys/block/mmcblk1" ]; then
+    search "/sys/block/mmcblk1/mmcblk1p*"
+fi
+if [ -d "/sys/block/sda" ]; then
+    search "/sys/block/sda/sda*"
+fi
