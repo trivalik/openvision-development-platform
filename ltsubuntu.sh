ab799158188b8b20c77ac7ee9d8d906d810e2754
Persian Prince <persianpros@yahoo.com>
2020-07-27 21:37:49 +0430

    ltsubuntu.sh: Install GCC 10 which is needed for newer OE!

--- ltsubuntu.sh
+++ ltsubuntu.sh
@@ -31,6 +31,15 @@ echo -e "Now install what we need, answer yes and wait for complete download"
 echo -e ""
 sudo apt-get install -y autoconf automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git git-lfs gzip help2man ncurses-bin libncurses5-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget chrpath libxml2-utils xsltproc python-setuptools libc6 genromfs mtd-utils dpkg-dev sshpass poedit translate-toolkit xclip linux-firmware linux-headers-`uname -r` linux-headers-`uname -r` linux-image-`uname -r` linux-tools-`uname -r` linux-libc-dev linux-source u-boot-tools upx-ucl doxygen repo optipng python-dev libglib2.0-dev pngquant default-jdk android-tools-fastboot android-tools-fsutils libssl-dev libc6-dev-i386 lib32z1 m4 liborbit2-dev intltool ccache zlib1g zlib1g-dev liblzo2-dev tcl dpkg asciidoc texlive-latex-base dblatex xutils-dev gparted openssh-server nfs-common nfs-kernel-server lintian git-doc git-gui gitk indent tofrodos fakeroot meld atftpd sharutils manpages-dev manpages-posix manpages-posix-dev libgdk-pixbuf2.0-dev linux-doc python-autopep8 build-essential socat libsdl1.2-dev xterm gcc-multilib libopenmpi-dev parted mercurial binutils imagemagick librsvg2-bin jq
 echo -e ""
+echo -e "Add GCC 10 to Ubuntu which is needed for newer OE!"
+echo -e ""
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update
+sudo apt install -y gcc-10 g++-10
+sudo apt-get update --fix-missing
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+echo -e ""
 echo -e "Remove useless packages ..."
 echo -e ""
 sudo apt autoremove -y
