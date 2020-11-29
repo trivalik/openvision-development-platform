957b153dedc6ba4ef8739d3e869c50a869a0ecb0
persianpros <persianpros@gmail.com>
2020-11-29 04:24:05 +0330

    openvision-module: Reduce the code with ov_kernel_info function

--- meta-openvision/recipes-openvision/openvision-module/openvision-module/openvision.c
+++ meta-openvision/recipes-openvision/openvision-module/openvision-module/openvision.c
@@ -26,6 +26,24 @@ static struct proc_dir_entry *proc_parent2;
 
 DEFINE_MUTEX(openvision_table_mutex);
 
+void ov_kernel_info(void)
+{
+	printk(KERN_INFO "Open Vision @VISIONVERSION@-@VISIONREVISION@\n");
+	printk(KERN_INFO "STB=@MACHINE@\n");
+	printk(KERN_INFO "Brand/Meta=@BOX_BRAND@\n");
+	printk(KERN_INFO "Platform=@STB_PLATFORM@\n");
+	printk(KERN_INFO "creator=Open Vision developers\n");
+	printk(KERN_INFO "url=https://openvision.tech\n");
+	printk(KERN_INFO "catalog=https://github.com/OpenVisionE2\n");
+	printk(KERN_INFO "visionlanguage=@VISIONLANGUAGE@\n");
+	printk(KERN_INFO "oe=@BUILD_VERSION@\n");
+	printk(KERN_INFO "python=@PREFERRED_VERSION_python@\n");
+	printk(KERN_INFO "multilib=@HAVE_MULTILIB@\n");
+	printk(KERN_INFO "architecture=@DEFAULTTUNE@\n");
+	printk(KERN_INFO "socfamily=@SOC_FAMILY@\n");
+	printk(KERN_INFO "compiledby=@DEVELOPER_NAME@\n");
+}
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,10,1)
 static int openvision_read_proc (char *page, char **start, off_t off, int count, int *eof, void *data_unused)
 {
@@ -44,20 +62,7 @@ static int openvision_read_proc (char *page, char **start, off_t off, int count,
 
 static int __init init_openvision(void)
 {
-	printk(KERN_INFO "Open Vision @VISIONVERSION@-@VISIONREVISION@\n");
-	printk(KERN_INFO "STB=@MACHINE@\n");
-	printk(KERN_INFO "Brand/Meta=@BOX_BRAND@\n");
-	printk(KERN_INFO "Platform=@STB_PLATFORM@\n");
-	printk(KERN_INFO "creator=Open Vision developers\n");
-	printk(KERN_INFO "url=https://openvision.tech\n");
-	printk(KERN_INFO "catalog=https://github.com/OpenVisionE2\n");
-	printk(KERN_INFO "visionlanguage=@VISIONLANGUAGE@\n");
-	printk(KERN_INFO "oe=@BUILD_VERSION@\n");
-	printk(KERN_INFO "python=@PREFERRED_VERSION_python@\n");
-	printk(KERN_INFO "multilib=@HAVE_MULTILIB@\n");
-	printk(KERN_INFO "architecture=@DEFAULTTUNE@\n");
-	printk(KERN_INFO "socfamily=@SOC_FAMILY@\n");
-	printk(KERN_INFO "compiledby=@DEVELOPER_NAME@\n");
+	ov_kernel_info();
 	proc_parent1 = proc_mkdir(dirname1, NULL);
 	proc_parent2 = proc_mkdir(dirname2, proc_parent1);
 	if ((proc_openvision = create_proc_entry(OV_MODULE_NAME, OV_PROC_PERMISSION, proc_parent2)))
@@ -93,26 +98,14 @@ static const struct file_operations proc_fops = {
 #endif
 static int __init init_openvision(void)
 {
-	printk(KERN_INFO "Open Vision @VISIONVERSION@-@VISIONREVISION@\n");
-	printk(KERN_INFO "STB=@MACHINE@\n");
-	printk(KERN_INFO "Brand/Meta=@BOX_BRAND@\n");
-	printk(KERN_INFO "Platform=@STB_PLATFORM@\n");
-	printk(KERN_INFO "creator=Open Vision developers\n");
-	printk(KERN_INFO "url=https://openvision.tech\n");
-	printk(KERN_INFO "catalog=https://github.com/OpenVisionE2\n");
-	printk(KERN_INFO "visionlanguage=@VISIONLANGUAGE@\n");
-	printk(KERN_INFO "oe=@BUILD_VERSION@\n");
-	printk(KERN_INFO "python=@PREFERRED_VERSION_python@\n");
-	printk(KERN_INFO "multilib=@HAVE_MULTILIB@\n");
-	printk(KERN_INFO "architecture=@DEFAULTTUNE@\n");
-	printk(KERN_INFO "socfamily=@SOC_FAMILY@\n");
-	printk(KERN_INFO "compiledby=@DEVELOPER_NAME@\n");
+	ov_kernel_info();
 	proc_parent1 = proc_mkdir(dirname1, NULL);
 	proc_parent2 = proc_mkdir(dirname2, proc_parent1);
 	proc_openvision = proc_create(OV_MODULE_NAME, OV_PROC_PERMISSION, proc_parent2, &proc_fops);
         return 0;
 }
 #endif
+
 module_init(init_openvision);
 
 MODULE_LICENSE("GPL");
